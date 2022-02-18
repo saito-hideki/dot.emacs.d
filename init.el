87d2093b5faa0127b5510f940c72e3e80bc019be
Hideki Saito <saito@fgrep.org>
2022-02-18 18:12:38 +0900

    Add terraform mode settings
    Signed-off-by: Hideki Saito <saito@fgrep.org>

--- init.el
+++ init.el
@@ -160,6 +160,11 @@
                             ))
 (add-hook 'highlight-indentation-mode-hook 'highlight-indentation-current-column-mode)
 
+
+;; Terraform settings
+(require 'terraform-mode)
+(add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+
 ;; ----------------------------------------
 ;; Following lines are automatically added
 (custom-set-variables
@@ -168,7 +173,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(spaceline highlight-indent-guides material-theme elpy exec-path-from-shell afternoon-theme flatland-theme cyberpunk-theme color-theme-sanityinc-solarized flatland-black-theme ddskk yaml-mode markdown-preview-mode json-mode ini-mode)))
+   '(terraform-mode spaceline highlight-indent-guides material-theme elpy exec-path-from-shell afternoon-theme flatland-theme cyberpunk-theme color-theme-sanityinc-solarized flatland-black-theme ddskk yaml-mode markdown-preview-mode json-mode ini-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
